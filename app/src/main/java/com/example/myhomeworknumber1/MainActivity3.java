b3533ecd0b4f78b4112f48e1b19ae007f9104815
Mishavw <mishaklimov153@gmail.com>
2022-07-06 19:55:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myhomeworknumber1/MainActivity3.java
@@ -0,0 +1,26 @@
+package com.example.myhomeworknumber1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationNewUserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Button btnGoToMain = findViewById(R.id.btnGoToMain);
+        btnGoToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHome = new Intent(RegistrationNewUserActivity.this, MainActivity.class);
+                startActivity(intentHome);
+            }
+        });
+
+    }
+}
